5b31c41f9198910d6f8eb4e8c2238715832a6a2b
ezio341 <argadiaz09@gmail.com>
2023-01-09 19:48:18 +0700

    fixed relative url path

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Dashboard from './pages/Dashboard'
 import DefaultLayout from './layouts/default';
 
 import {
+  Navigate,
   useRoutes,
 } from 'react-router-dom'
 import RequireAuth from './components/RequireAuth';
 import Login from './pages/Login';
 
 function App() {
+  let relativePath = process.env.REACT_APP_VERSION
   let routes = useRoutes([
-    {path: '/login', element: <Login />},
+    {path:'/', element: <Navigate to={`/${relativePath}`}/>},
+    {path: `/${relativePath}/login`, element: <Login />},
     {element: <DefaultLayout />, children:[
       {element: <RequireAuth />, children:[
-        {path: '/', element:<Dashboard />}
+        {path: `/${relativePath}`, element:<Dashboard />}
       ]}
     ]}
   ])
